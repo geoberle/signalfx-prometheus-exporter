43149b66eb518a745a2bd894f43dbeb5d8d35913
Gerd Oberlechner <goberlec@redhat.com>
2022-02-27 12:07:01 +0100

    add prometheus registry filtering wrapper
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

---
+++ sfxpe/registry.go
@@ -0,0 +1,42 @@
+package sfxpe
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+)
+
+type FilteringRegistry struct {
+	Registry   prometheus.Gatherer
+	LabelName  string
+	LabelValue string
+}
+
+func (fr *FilteringRegistry) Gather() ([]*dto.MetricFamily, error) {
+	mfs, err := fr.Registry.Gather()
+	if err != nil {
+		return nil, err
+	}
+
+	filteredMfs := []*dto.MetricFamily{}
+	for _, mf := range mfs {
+		metrics := []*dto.Metric{}
+		for _, m := range mf.GetMetric() {
+			for _, l := range m.GetLabel() {
+				if *l.Name == fr.LabelName && *l.Value == fr.LabelValue {
+					metrics = append(metrics, m)
+					break
+				}
+			}
+		}
+		if len(metrics) > 0 {
+			filteredMfs = append(filteredMfs, &dto.MetricFamily{
+				Name:   mf.Name,
+				Help:   mf.Help,
+				Type:   mf.Type,
+				Metric: metrics,
+			})
+		}
+	}
+
+	return filteredMfs, nil
+}
