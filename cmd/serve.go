3f780a1ea6281065fca5a44361ae87c9b737cd9f
Gerd Oberlechner <goberlec@redhat.com>
2022-02-28 00:04:34 +0100

    serve command for scrape target with filtering
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

---
+++ cmd/serve.go
@@ -0,0 +1,158 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"signalfx-prometheus-exporter/config"
+	"signalfx-prometheus-exporter/sfxpe"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/signalfx/signalfx-go/signalflow"
+	"github.com/signalfx/signalfx-go/signalflow/messages"
+	"github.com/spf13/cobra"
+)
+
+var (
+	listenPort int
+	configFile string
+	registry   *prometheus.Registry
+	gauges     map[string]*prometheus.GaugeVec
+)
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Listen for signalfx scrape requests",
+	Run: func(cmd *cobra.Command, args []string) {
+		flowPrograms, err := config.LoadFlowPrograms(configFile)
+		if err != nil {
+			log.Fatalf("failed to load config: %+s", err)
+			return
+		}
+
+		mux := http.NewServeMux()
+		mux.HandleFunc("/probe", probeHandler)
+		server := &http.Server{Addr: fmt.Sprintf(":%v", listenPort), Handler: mux}
+
+		registry = prometheus.NewRegistry()
+		gauges = make(map[string]*prometheus.GaugeVec)
+
+		for _, fp := range flowPrograms {
+			go func(fp config.FlowProgram) {
+				streamData(fp)
+			}(fp)
+		}
+
+		go func() {
+			if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				log.Fatalf("listen:%+s\n", err)
+			}
+		}()
+		log.Printf("Listening on port %v\n", listenPort)
+
+		<-cmd.Context().Done()
+
+		log.Printf("Server stopped")
+
+		ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer func() {
+			cancel()
+		}()
+
+		if err := server.Shutdown(ctxShutDown); err != nil {
+			log.Fatalf("server Shutdown Failed:%+s", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+	serveCmd.Flags().IntVarP(&listenPort, "port", "p", 1236, "listen port for incoming scrape requests")
+	serveCmd.Flags().StringVarP(&configFile, "config", "c", "", "flow config file")
+
+}
+
+func probeHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), time.Duration(5*float64(time.Second)))
+	defer cancel()
+	r = r.WithContext(ctx)
+
+	var metricGatherer prometheus.Gatherer
+	matchQueries, ok := r.URL.Query()["match"]
+	if ok && len(matchQueries) > 0 {
+		metricGatherer = &sfxpe.FilteringRegistry{
+			Registry:       registry,
+			VectorSelector: matchQueries[0],
+		}
+	} else {
+		metricGatherer = registry
+	}
+
+	h := promhttp.HandlerFor(metricGatherer, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
+}
+
+func streamData(fp config.FlowProgram) {
+	client, _ := signalflow.NewClient(
+		signalflow.StreamURLForRealm(fp.Sfx.Realm),
+		signalflow.AccessToken(fp.Sfx.Token),
+	)
+
+	comp, _ := client.Execute(&signalflow.ExecuteRequest{
+		Program: fp.Query,
+	})
+
+	for msg := range comp.Data() {
+		if len(msg.Payloads) == 0 {
+			continue
+		}
+		for _, pl := range msg.Payloads {
+			meta := comp.TSIDMetadata(pl.TSID)
+			gauge, err := getGauge(fp.Metric, meta)
+			if err != nil {
+				// todo log
+			} else {
+				gauge.Set(pl.Float64())
+			}
+		}
+	}
+}
+
+func getGauge(metric *config.PrometheusMetric, sfxMeta *messages.MetadataProperties) (prometheus.Gauge, error) {
+	// build name
+	name, err := metric.GetMetricName(sfxMeta)
+	if err != nil {
+		return nil, err
+	}
+	name = strings.ReplaceAll(name, ".", "_")
+
+	// build labels
+	labelNames := make([]string, len(metric.Labels))
+	labelValues := make([]string, len(metric.Labels))
+	var i = 0
+	for name := range metric.Labels {
+		labelNames[i] = name
+		value, err := metric.GetLabelValue(name, sfxMeta)
+		if err != nil {
+			return nil, err
+		}
+		labelValues[i] = value
+		i++
+	}
+
+	// build  or reuse gauge
+	g, ok := gauges[name]
+	if !ok {
+		g = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Name: name,
+		}, labelNames)
+		gauges[name] = g
+		registry.MustRegister(g)
+	}
+	return g.WithLabelValues(labelValues...), nil
+}
--- config/config.go
+++ config/config.go
@@ -1,15 +1,58 @@
 package config
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"text/template"
 
 	"gopkg.in/yaml.v2"
 )
 
 type PrometheusMetric struct {
-	Name   string            `yaml:"name"`
-	Labels map[string]string `yaml:"labels"`
+	Name           string            `yaml:"name"`
+	Labels         map[string]string `yaml:"labels"`
+	nameTemplate   *template.Template
+	labelTemplates map[string]*template.Template
+}
+
+func (pm *PrometheusMetric) Validate() error {
+	// name template
+	tmpl, err := template.New("x").Parse(pm.Name)
+	if err != nil {
+		return err
+	}
+	pm.nameTemplate = tmpl
+
+	// label templates
+	labelTemplates := map[string]*template.Template{}
+	for labelName, labelValue := range pm.Labels {
+		tmpl, err := template.New("x").Parse(labelValue)
+		if err != nil {
+			return err
+		}
+		labelTemplates[labelName] = tmpl
+	}
+	pm.labelTemplates = labelTemplates
+
+	return nil
+}
+
+func (pm *PrometheusMetric) GetMetricName(data interface{}) (string, error) {
+	var buffer bytes.Buffer
+	err := pm.nameTemplate.Execute(&buffer, data)
+	return buffer.String(), err
+}
+
+func (pm *PrometheusMetric) GetLabelValue(labelName string, data interface{}) (string, error) {
+	tmpl, ok := pm.labelTemplates[labelName]
+	if !ok {
+		return "", fmt.Errorf("Could not find label named %s", labelName)
+	}
+	var buffer bytes.Buffer
+	err := tmpl.Execute(&buffer, data)
+	return buffer.String(), err
 }
 
 type FlowProgram struct {
@@ -26,7 +69,11 @@ type FlowProgram struct {
 			Values   []string `yaml:"values"`
 		} `yaml:"matchExpressions"`
 	} `yaml:"selector"`
-	Metric PrometheusMetric `yaml:"prometheusMetric"`
+	Metric *PrometheusMetric `yaml:"prometheusMetric"`
+}
+
+func (fp *FlowProgram) Validate() error {
+	return fp.Metric.Validate()
 }
 
 func LoadFlowPrograms(file string) ([]FlowProgram, error) {
@@ -35,6 +82,7 @@ func LoadFlowPrograms(file string) ([]FlowProgram, error) {
 		log.Printf("yamlFile.Get err   #%v ", err)
 		return nil, err
 	}
+
 	flowProgramList := struct {
 		Flows []FlowProgram `yaml:"flows"`
 	}{}
@@ -42,6 +90,12 @@ func LoadFlowPrograms(file string) ([]FlowProgram, error) {
 	if err != nil {
 		log.Fatalf("Unmarshal: %v", err)
 		return nil, err
+	}
+
+	for _, fp := range flowProgramList.Flows {
+		if err := fp.Validate(); err != nil {
+			return nil, err
+		}
 	}
 
 	return flowProgramList.Flows, nil
