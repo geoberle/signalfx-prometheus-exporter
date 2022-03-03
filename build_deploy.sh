4f5dfd4fd28178f862b9c7b0be7f1f619719cb43
Gerd Oberlechner <goberlec@redhat.com>
2022-03-03 15:59:19 +0100

    moved hack script to root (#10)
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -4,6 +4,5 @@ DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
-# build & push image
-make build 
-#push
+# build images
+make build push
